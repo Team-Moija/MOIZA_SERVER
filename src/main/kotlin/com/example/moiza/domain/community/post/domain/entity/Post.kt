a8e120474ae5c73559dc05b92947195d01e145bb
김명진 <4mj.eo@dsm.hs.kr>
2024-10-16 19:33:17 +0900

    refactor(hashtag): 엔티티 관계 수정

--- src/main/kotlin/com/example/moiza/domain/community/post/domain/entity/Post.kt
+++ src/main/kotlin/com/example/moiza/domain/community/post/domain/entity/Post.kt
@@ -3,10 +3,8 @@ package com.example.moiza.domain.community.post.domain.entity
 import com.example.moiza.domain.community.community.domain.entity.Community
 import com.example.moiza.domain.community.community.domain.entity.type.CommunityType
 import com.example.moiza.domain.user.domain.User
-import jakarta.persistence.CascadeType
 import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
-import jakarta.persistence.OneToMany
 
 @Entity(name = "post")
 class Post(
@@ -23,9 +21,5 @@ class Post(
 
     @ElementCollection
     var image: List<String>? = image
-        protected set
-
-    @OneToMany(mappedBy = "post", cascade = [CascadeType.ALL], orphanRemoval = true)
-    var postHashtags: MutableList<PostHashtag> = mutableListOf()
         protected set
 }
